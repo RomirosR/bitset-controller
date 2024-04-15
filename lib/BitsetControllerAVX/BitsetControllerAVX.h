12e26af190e14954ed9d3e0ab9568ae5e79f2605
RomirosR <ahkakok@gmail.com>
2024-04-16 01:49:10 +0300

    [tests failed] Add fixed-size BitsetControllerAVX

--- lib/BitsetControllerAVX/BitsetControllerAVX.h
+++ lib/BitsetControllerAVX/BitsetControllerAVX.h
@@ -2,13 +2,16 @@
 
 #include "immintrin.h"
 #include <algorithm>
+#include <array>
+#include <cstdint>
 
 class BitsetControllerAVX{
    private:
-    __m512i a_;
+    std::array<__m256i, 2> a_;
+    std::array<int64_t, 4> find_temp;
 
    public:
-    BitsetControllerAVX() : a_(_mm512_setzero()){
+    BitsetControllerAVX() : a_({_mm256_setzero_si256(), _mm256_setzero_si256()}) {
     }
 
     BitsetControllerAVX(const BitsetControllerAVX& other) = default;
@@ -17,5 +20,117 @@ class BitsetControllerAVX{
 
     BitsetControllerAVX& operator=(const BitsetControllerAVX& other) = default;
 
-    [[noexcept]] BitsetControllerAVX(BitsetControllerAVX&& other) = default;
+    [[noexcept]] BitsetControllerAVX& operator=(BitsetControllerAVX&& other) = default;
+
+    static __int64_t Get(__m256i a, std::size_t i) {
+        if (i == 0) {
+            return _mm256_extract_epi64(a, 0);
+        }
+        if (i == 1) {
+            return _mm256_extract_epi64(a, 1);
+        }
+        if (i == 2) {
+            return _mm256_extract_epi64(a, 2);
+        }
+        if (i == 3) {
+            return _mm256_extract_epi64(a, 3);
+        }
+        return -1;
+    }
+
+    static __m256i Set(__m256i a, std::size_t i, __int64_t x) {
+        if (i == 0) {
+            return _mm256_insert_epi64(a, x, 0);
+        }
+        if (i == 1) {
+            return _mm256_insert_epi64(a, x, 1);
+        }
+        if (i == 2) {
+            return _mm256_insert_epi64(a, x, 2);
+        }
+        if (i == 3) {
+            return _mm256_insert_epi64(a, x, 3);
+        }
+        return _mm256_setzero_si256();
+    }
+
+    void Add(std::size_t i) {
+        const int c = i >> 8; // 0 <= c <= 1
+        i &= 255; // 0 <= i <= 255
+        __int64_t x = Get(a_[c], i >> 6);
+        x |= (1ull << (i & 63));
+        a_[c] = Set(a_[c], i >> 6, x);
+    }
+
+    bool Test(std::size_t i) {
+        const int c = i >> 8; // 0 <= c <= 1
+        i &= 255; // 0 <= i <= 255
+        __int64_t x = Get(a_[c], i >> 6);
+        return  (x & (1ull << (i & 63))) != 0;
+    }
+
+    BitsetControllerAVX& operator|=(const BitsetControllerAVX& other) {
+        for (int i = 0; i < 2; i++) {
+            a_[i] = _mm256_or_si256(a_[i], other.a_[i]);
+        }
+        return *this;
+    }
+
+    BitsetControllerAVX operator|(const BitsetControllerAVX& other) {
+        BitsetControllerAVX temp = *this;
+        temp |= other;
+        return temp;
+    }
+
+    BitsetControllerAVX& operator&=(const BitsetControllerAVX& other) {
+        for (int i = 0; i < 2; i++) {
+            a_[i] = _mm256_and_si256(a_[i], other.a_[i]);
+        }
+        return *this;
+    }
+
+    BitsetControllerAVX operator&(const BitsetControllerAVX& other) {
+        BitsetControllerAVX temp = *this;
+        temp |= other;
+        return temp;
+    }
+
+    std::size_t Find_next(std::size_t i) {
+        for (int j = 0; j < 4; j++) {
+            find_temp[j] = Get(a_[i >> 8], j);
+        }
+        int j = 0;
+        while (j < 4) {
+            if (((i >> 8) << 8) + (j << 6) <= i) {
+                find_temp[j] = 0;
+            }
+            if (((i >> 8) << 8) <= i && i < ((i >> 8) << 8) + (j << 6)) {
+                find_temp[j] = find_temp[j] & (~((1ull << (i & 63)) - 1));
+                break;
+            }
+            j++;
+        }
+        __int64_t or_val = 0;
+        for (int k = j; k < 4; k++) {
+            or_val |= find_temp[k];
+        }
+        if (or_val != 0) {
+            while (j < 4 && find_temp[j] == 0) {
+                j++;
+            }
+            return ((i >> 8) << 8) + (j << 6) + __builtin_ctzll(find_temp[j]);
+        }
+        i += 256 - (i & 255);
+        while (i < 512 && _mm256_testz_si256(a_[i >> 8], a_[i >> 8])) {
+            i += 256;
+        }
+        if (i >= 512) {
+            return 512;
+        }
+        j = 0;
+        while (j < 4 && Get(a_[i >> 8], j) == 0) {
+            j++;
+        }
+        return ((i >> 8) << 8) + (j << 6) + __builtin_ctzll(Get(a_[i >> 8], j));
+    } 
 };
--- tests/lib/BitsetControllerAVX/test.cpp
+++ tests/lib/BitsetControllerAVX/test.cpp
@@ -12,106 +12,105 @@ const std::size_t SIZE = 512;
 
 int RandFromRange(int l, int r) { return rng() % (r - l + 1) + l; }
 
-// std::pair<BitsetControllerAVX, std::vector<bool>> RandomBitsetAndActual() {
-//     BitsetControllerAVX bc;
-//     const int k = RandFromRange(0, SIZE);
-//     std::vector<bool> actual(SIZE);
-//     for (int i = 0; i < k; i++) {
-//         const int idx = RandFromRange(0, SIZE - 1);
-//         bc.Add(idx);
-//         actual[idx] = true;
-//     }
-//     return {bc, actual};
-// }
+std::pair<BitsetControllerAVX, std::vector<bool>> RandomBitsetAndActual() {
+    BitsetControllerAVX bc;
+    const int k = RandFromRange(0, SIZE);
+    std::vector<bool> actual(SIZE);
+    for (int i = 0; i < k; i++) {
+        const int idx = RandFromRange(0, SIZE - 1);
+        bc.Add(idx);
+        actual[idx] = true;
+    }
+    return {bc, actual};
+}
 
-// TEST_CASE("Add and Test methods") {
-//     BitsetControllerAVX bc;
-//     for (int i = 0; i < 100; i++) {
-//         bc = BitsetControllerAVX();
-//         bc.Add(i);
-//         REQUIRE(bc.Test(i));
-//         for (int j = 0; j < SIZE; j++) {
-//             if (i == j) {
-//                 continue;
-//             }
-//             std::cout << j << " ";
-//             REQUIRE(!bc.Test(j));
-//         }
-//     }
-//     for (int i = 0; i < 100; i++) {
-//         bc = BitsetControllerAVX();
-//         std::vector<bool> actual(SIZE);
-//         const int k = RandFromRange(0, SIZE);
-//         for (int j = 0; j < k; j++) {
-//             const int idx = RandFromRange(0, SIZE - 1);
-//             actual[idx] = true;
-//             bc.Add(idx);
-//         }
-//         for (int j = 0; j < SIZE; j++) {
-//             REQUIRE(actual[j] == bc.Test(j));
-//         }
-//     }
-// }
+TEST_CASE("Add and Test methods") {
+    BitsetControllerAVX bc;
+    for (int i = 0; i < 100; i++) {
+        bc = BitsetControllerAVX();
+        bc.Add(i);
+        REQUIRE(bc.Test(i));
+        for (int j = 0; j < SIZE; j++) {
+            if (i == j) {
+                continue;
+            }
+            REQUIRE(!bc.Test(j));
+        }
+    }
+    for (int i = 0; i < 100; i++) {
+        bc = BitsetControllerAVX();
+        std::vector<bool> actual(SIZE);
+        const int k = RandFromRange(0, SIZE);
+        for (int j = 0; j < k; j++) {
+            const int idx = RandFromRange(0, SIZE - 1);
+            actual[idx] = true;
+            bc.Add(idx);
+        }
+        for (int j = 0; j < SIZE; j++) {
+            REQUIRE(actual[j] == bc.Test(j));
+        }
+    }
+}
 
-// TEST_CASE("Bitwise OR") {
-//     for (int i = 0; i < 100; i++) {
-//         auto [bc1, actual1] = RandomBitsetAndActual();
-//         auto [bc2, actual2] = RandomBitsetAndActual();
-//         auto bc3 = bc1 | bc2;
-//         for (int j = 0; j < SIZE; j++) {
-//             REQUIRE((bc1.Test(j) | bc2.Test(j)) == (actual1[j] | actual2[j]));
-//         }
-//     }
-//     for (int i = 0; i < 100; i++) {
-//         auto [bc1, actual1] = RandomBitsetAndActual();
-//         auto [bc2, actual2] = RandomBitsetAndActual();
-//         bc1 |= bc2;
-//         for (int j = 0; j < SIZE; j++) {
-//             REQUIRE((bc1.Test(j)) == (actual1[j] | actual2[j]));
-//         }
-//     }
-// }
+TEST_CASE("Bitwise OR") {
+    for (int i = 0; i < 100; i++) {
+        auto [bc1, actual1] = RandomBitsetAndActual();
+        auto [bc2, actual2] = RandomBitsetAndActual();
+        auto bc3 = bc1 | bc2;
+        for (int j = 0; j < SIZE; j++) {
+            REQUIRE((bc1.Test(j) | bc2.Test(j)) == (actual1[j] | actual2[j]));
+        }
+    }
+    for (int i = 0; i < 100; i++) {
+        auto [bc1, actual1] = RandomBitsetAndActual();
+        auto [bc2, actual2] = RandomBitsetAndActual();
+        bc1 |= bc2;
+        for (int j = 0; j < SIZE; j++) {
+            REQUIRE((bc1.Test(j)) == (actual1[j] | actual2[j]));
+        }
+    }
+}
 
-// TEST_CASE("Bitwise AND") {
-//     for (int i = 0; i < 100; i++) {
-//         auto [bc1, actual1] = RandomBitsetAndActual();
-//         auto [bc2, actual2] = RandomBitsetAndActual();
-//         auto bc3 = bc1 & bc2;
-//         for (int j = 0; j < SIZE; j++) {
-//             REQUIRE((bc1.Test(j) & bc2.Test(j)) == (actual1[j] & actual2[j]));
-//         }
-//     }
-//     for (int i = 0; i < 100; i++) {
-//         auto [bc1, actual1] = RandomBitsetAndActual();
-//         auto [bc2, actual2] = RandomBitsetAndActual();
-//         bc1 &= bc2;
-//         for (int j = 0; j < SIZE; j++) {
-//             REQUIRE((bc1.Test(j)) == (actual1[j] & actual2[j]));
-//         }
-//     }
-// }
+TEST_CASE("Bitwise AND") {
+    for (int i = 0; i < 100; i++) {
+        auto [bc1, actual1] = RandomBitsetAndActual();
+        auto [bc2, actual2] = RandomBitsetAndActual();
+        auto bc3 = bc1 & bc2;
+        for (int j = 0; j < SIZE; j++) {
+            REQUIRE((bc1.Test(j) & bc2.Test(j)) == (actual1[j] & actual2[j]));
+        }
+    }
+    for (int i = 0; i < 100; i++) {
+        auto [bc1, actual1] = RandomBitsetAndActual();
+        auto [bc2, actual2] = RandomBitsetAndActual();
+        bc1 &= bc2;
+        for (int j = 0; j < SIZE; j++) {
+            REQUIRE((bc1.Test(j)) == (actual1[j] & actual2[j]));
+        }
+    }
+}
 
-// TEST_CASE("Find_next method") {
-//     for (int it = 0; it < 100; it++) {
-//         auto [bc, actual] = RandomBitsetAndActual();
-//         int cnt = 0;
-//         std::vector<bool> cur(SIZE);
-//         for (int i = bc.Find_next(0); i < SIZE; i = bc.Find_next(i + 1)) {
-//             cnt++;
-//             cur[i] = true;
-//         }
-//         REQUIRE(actual == cur);
-//         REQUIRE(cnt == std::count(actual.begin(), actual.end(), true));
-//     }
-//     for (int it1 = 0; it1 < 100; it1++) {
-//         auto [bc, actual] = RandomBitsetAndActual();
-//         for (int it2 = 0; it2 < 50; it2++) {
-//             const auto idx = static_cast<std::size_t>(RandFromRange(0, SIZE - 1));
-//             auto actual_idx = idx;
-//             while (actual_idx < SIZE && !actual[actual_idx]) {
-//                 actual_idx++;
-//             }
-//             REQUIRE(actual_idx == bc.Find_next(idx));
-//         }
-//     }
-// }
+TEST_CASE("Find_next method") {
+    for (int it = 0; it < 100; it++) {
+        auto [bc, actual] = RandomBitsetAndActual();
+        int cnt = 0;
+        std::vector<bool> cur(SIZE);
+        for (int i = bc.Find_next(0); i < SIZE; i = bc.Find_next(i + 1)) {
+            cnt++;
+            cur[i] = true;
+        }
+        REQUIRE(actual == cur);
+        REQUIRE(cnt == std::count(actual.begin(), actual.end(), true));
+    }
+    for (int it1 = 0; it1 < 100; it1++) {
+        auto [bc, actual] = RandomBitsetAndActual();
+        for (int it2 = 0; it2 < 50; it2++) {
+            const auto idx = static_cast<std::size_t>(RandFromRange(0, SIZE - 1));
+            auto actual_idx = idx;
+            while (actual_idx < SIZE && !actual[actual_idx]) {
+                actual_idx++;
+            }
+            REQUIRE(actual_idx == bc.Find_next(idx));
+        }
+    }
+}
